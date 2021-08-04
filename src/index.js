0be35d5e71dd0d7e336627d06e4e45ee923ac47f
Tijani Fulani <Fulani07@gmail.com>
2021-08-04 15:15:14 +0100

    database connected

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const db = require('./utilities/database');
 
-const app = express()
+const app = express();
 
 // Middleware
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan('dev'));
+app.use(express.json());
 
 // Routes
 
-
 // Listen to server
+app.listen(4000, () => {
+  db.connect((error) => {
+    if (error) {
+      console.error('[ERROR] Connection error: ', error.stack);
+    } else {
+      console.log('\n[DB] Connected....\n');
+    }
+  });
 
-app.listen(4000, ()=> {
- console.log('Server is running on port 4000');
-})
+  console.log('Server is running on port 4000');
+});
